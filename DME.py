25bc0822e69671104ec9a8bdbfab9d5173b50ab8
YezhenWang <309426989@qq.com>
2019-01-13 16:28:51 +0800

    16:28 1.13 2019 in harbin snow

--- DME.py
+++ DME.py
@@ -5,21 +5,21 @@ import tensorflow.contrib.slim.nets as nets
 model_path = '/home/zzn/PycharmProjects/ADCrowdNet/vgg_pre-trained_variable_list/vgg_16.ckpt'
 
 
-def DME_front_end(features):
-    logits, end_points = nets.vgg.vgg_16(features)
-    assert (os.path.isfile(model_path))
-    variables_to_restore = slim.contrib_framework.get_variables_to_restore(
-        include=['vgg_16/conv1',
-                 'vgg_16/pool1',
-                 'vgg_16/conv2',
-                 'vgg_16/pool2',
-                 'vgg_16/conv3',
-                 'vgg_16/pool3',
-                 'vgg_16/conv4'
-                 ])
-    init_fn = slim.contrib_framework.assign_from_checkpoint_fn(model_path, variables_to_restore)
-    front_end = end_points['vgg_16/conv4/conv4_3']
-    return front_end, init_fn
+# def DME_front_end(features):
+#     logits, end_points = nets.vgg.vgg_16(features)
+#     assert (os.path.isfile(model_path))
+#     variables_to_restore = slim.contrib_framework.get_variables_to_restore(
+#         include=['vgg_16/conv1',
+#                  'vgg_16/pool1',
+#                  'vgg_16/conv2',
+#                  'vgg_16/pool2',
+#                  'vgg_16/conv3',
+#                  'vgg_16/pool3',
+#                  'vgg_16/conv4'
+#                  ])
+#     init_fn = slim.contrib_framework.assign_from_checkpoint_fn(model_path, variables_to_restore)
+#     front_end = end_points['vgg_16/conv4/conv4_3']
+#     return front_end, init_fn
 
 
 def DME_inception(features, index,  output_nums):
@@ -30,22 +30,25 @@ def DME_inception(features, index,  output_nums):
     output = tf.concat([part_1, part_2, part_3], axis=1, name='concat_' + str(index))
     return output
 
+
 def DME_back_end(features):
     features_transpose = tf.transpose(features, [0, 3, 1, 2])
-    with slim.arg_scope([slim.conv2d],
-                        weights_regularizer=slim.l2_regularizer(4e-4),
-                        activation_fn=tf.nn.relu):
+    with slim.arg_scope([slim.conv2d], activation_fn=tf.nn.relu):
         net = DME_inception(features_transpose, 1, 256)
         net = slim.conv2d(net, 256, 1, 1, data_format='NCHW', scope='conv1x1_1')
         net = DME_inception(net, 2, 128)
         net = slim.conv2d(net, 128, 1, 1, data_format='NCHW', scope='conv1x1_2')
         net = DME_inception(net, 3, 64)
         net = slim.conv2d(net, 1, 1, 1, data_format='NCHW', scope='conv1x1_3')
-    return net
+    output = tf.transpose(net, [0, 2, 3, 1])
+    return output
 
 
 def DME_model(features):
-    front_end, init_vgg16_fn = DME_front_end(features)
+    features = slim.instance_norm(features, activation_fn=tf.nn.relu)
+    # front_end, init_vgg16_fn = DME_front_end(features)
+    _, end_points = nets.vgg.vgg_16(features)
+    front_end = end_points['vgg_16/conv4/conv4_3']
     feature_map = DME_back_end(front_end)
-    output = tf.transpose(feature_map, [0, 2, 3, 1])
-    return output, init_vgg16_fn
+    # return output, init_vgg16_fn
+    return feature_map
